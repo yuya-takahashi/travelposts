f51328fc016c3821021c8d86e33a8851c4a01040
yuya.takahashi <Pochaty119@outlook.jp>
2020-11-11 07:32:33 +0000

    user login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center">
+        <h1>ログイン</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 offset-sm-3">
+
+            {!! Form::open(['route' => 'login.post']) !!}
+                <div class="form-group">
+                    {!! Form::label('email', 'メールアドレス') !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('password', 'パスワード') !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
+                </div>
+
+                {!! Form::submit('ログイン', ['class' => 'btn btn-primary btn-block']) !!}
+            {!! Form::close() !!}
+
+            {{-- ユーザ登録ページへのリンク --}}
+            <p class="mt-2">{!! link_to_route('signup.get', 'アカウントをお持ちでない方はこちら') !!}</p>
+        </div>
+    </div>
+@endsection
